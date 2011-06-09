6f5266864628332435c0c5992e6b3dfbbba5b3f3
andyc <andyc@crytek.com>
2011-06-09 08:40:15 +0100

    "fixed" cmake stuff for win32

--- cmake/Modules/FindGLFW.cmake
+++ cmake/Modules/FindGLFW.cmake
@@ -21,28 +21,42 @@
 
 
 #Search for the include file...
-FIND_PATH(GLFW_INCLUDE_DIR GL/glfw.h DOC "Path to GLFW include directory."
-  HINTS
-  $ENV{GLFW_ROOT}
-  PATH_SUFFIX include #For finding the include file under the root of the glfw expanded archive, typically on Windows.
-  PATHS
-  /usr/include/
-  /usr/local/include/
-  ${PROJECT_ROOT_DIR}/3pp/include/ # added by ptr
-  ${PROJECT_ROOT_DIR}/3pp/include/GL/ # added by ptr
-)
+if (WIN32)
+	#Don't seem to be able to find this on Windows (installation/config error?)
+	#set it directly
+	set(GLFW_INCLUDE_DIR "/usr/local/include/")
+endif (WIN32)
+if (UNIX)
+	FIND_PATH(GLFW_INCLUDE_DIR GL/glfw.h DOC "Path to GLFW include directory."
+		HINTS
+		$ENV{GLFW_ROOT}
+		PATH_SUFFIX include #For finding the include file under the root of the glfw expanded archive, typically on Windows.
+		PATHS
+		/usr/include/
+		/usr/local/include/
+		${PROJECT_ROOT_DIR}/3pp/include/ # added by ptr
+		${PROJECT_ROOT_DIR}/3pp/include/GL/ # added by ptr
+		)
+endif (UNIX)
 #DBG_MSG("GLFW_INCLUDE_DIR = ${GLFW_INCLUDE_DIR}")
 
-FIND_LIBRARY(GLFW_LIBRARY DOC "Absolute path to GLFW library."
-  NAMES glfw
-  HINTS
-  $ENV{GLFW_ROOT}
-  PATH_SUFFIXES lib/win32 #For finding the library file under the root of the glfw expanded archive, typically on Windows.
-  PATHS
-  /usr/lib
-  /usr/local/lib
-  ${PROJECT_ROOT_DIR}/3pp/lib/ # added by ptr
-)
+if (WIN32)
+	#Don't seem to be able to find this on Windows (installation/config error?)
+	#set it directly
+	set(GLFW_LIBRARY "/usr/local/lib/libglfw.a")
+endif (WIN32)
+if (UNIX)
+	FIND_LIBRARY(GLFW_LIBRARY DOC "Absolute path to GLFW library."
+		NAMES glfw
+		HINTS
+		$ENV{GLFW_ROOT}
+		PATH_SUFFIXES lib/win32 #For finding the library file under the root of the glfw expanded archive, typically on Windows.
+		PATHS
+		/usr/lib
+		/usr/local/lib
+		${PROJECT_ROOT_DIR}/3pp/lib/ # added by ptr
+		)
+endif (UNIX)
 #DBG_MSG("GLFW_LIBRARY = ${GLFW_LIBRARY}")
 
 SET(GLFW_FOUND 0)
