ba6da9f223cdd0a8589514d1305999a28fbeb6e9
Flapstah <flapstah@gmail.com>
2011-06-20 07:38:47 +0100

    fleshing out z80 stuff (wip)

--- z80.h
+++ z80.h
@@ -25,9 +25,12 @@ class CZ80
 		{
 			uint8 A;
 			uint8 F;
-			uint16 BC;
-			uint16 DE;
-			uint16 HL;
+			uint8 B;
+			uint8 C;
+			uint8 D;
+			uint8 E;
+			uint8 H;
+			uint8 L;
 			uint16 IX;
 			uint16 IY;
 			uint16 PC;
