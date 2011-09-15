afa5bd88dc00eecf3e732c9c37d7678edbebc8a7
andyc <andyc@crytek.com>
2011-09-15 13:44:06 +0100

    added some z80 test stuff

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ int main(int argc, char* argv[])
   // working
 	speccy.LoadSNA("roms/android2.sna");
 	speccy.LoadSNA("roms/hobbit.sna");
+	speccy.LoadSNA("roms/zexall.sna");
+	speccy.LoadRAW("roms/z80tests.raw");
+
 	// not working
 	speccy.LoadSNA("roms/LORDSMID.SNA");
 	speccy.LoadRAW("roms/manic.raw");
@@ -24,7 +27,7 @@ int main(int argc, char* argv[])
 	speccy.LoadRAW("roms/007Spy.raw");
 	speccy.LoadRAW("roms/Magnetron.raw");
 */
-	speccy.LoadRAW("roms/manic.raw");
+	speccy.LoadRAW("roms/z80tests.raw");
 	while (speccy.Update());
 
 	return EXIT_SUCCESS;
