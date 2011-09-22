33a1b8a84f1128569a3102fb3bf812712a90ca5a
Flapstah <flapstah@gmail.com>
2011-09-22 17:08:51 +0100

    fleshed out decode ED prefix

--- z80.cpp
+++ z80.cpp
@@ -1918,9 +1918,6 @@ uint32 CZ80::Step(void)
 					case 0xBD:
 					case 0xBE:
 					case 0xBF:
-						return ImplementNOP() + ImplementNOP();
-						break;
-
 					case 0xC0:
 					case 0xC1:
 					case 0xC2:
@@ -3293,6 +3290,73 @@ void CZ80::Decode(uint16& address, char* pMnemonic) const
 			case 0xED: // Prefix ED
 				switch (ReadMemory(address + 1))
 				{
+					case 0x00:
+					case 0x01:
+					case 0x02:
+					case 0x03:
+					case 0x04:
+					case 0x05:
+					case 0x06:
+					case 0x07:
+					case 0x08:
+					case 0x09:
+					case 0x0A:
+					case 0x0B:
+					case 0x0C:
+					case 0x0D:
+					case 0x0E:
+					case 0x0F:
+					case 0x10:
+					case 0x11:
+					case 0x12:
+					case 0x13:
+					case 0x14:
+					case 0x15:
+					case 0x16:
+					case 0x17:
+					case 0x18:
+					case 0x19:
+					case 0x1A:
+					case 0x1B:
+					case 0x1C:
+					case 0x1D:
+					case 0x1E:
+					case 0x1F:
+					case 0x20:
+					case 0x21:
+					case 0x22:
+					case 0x23:
+					case 0x24:
+					case 0x25:
+					case 0x26:
+					case 0x27:
+					case 0x28:
+					case 0x29:
+					case 0x2A:
+					case 0x2B:
+					case 0x2C:
+					case 0x2D:
+					case 0x2E:
+					case 0x2F:
+					case 0x30:
+					case 0x31:
+					case 0x32:
+					case 0x33:
+					case 0x34:
+					case 0x35:
+					case 0x36:
+					case 0x37:
+					case 0x38:
+					case 0x39:
+					case 0x3A:
+					case 0x3B:
+					case 0x3C:
+					case 0x3D:
+					case 0x3E:
+					case 0x3F:
+						DecodeNOP(address, pMnemonic);
+						break;
+
 					case 0x40: // IN B,(C)
 					case 0x50: // IN D,(C)
 					case 0x60: // IN H,(C)
@@ -3384,71 +3448,198 @@ void CZ80::Decode(uint16& address, char* pMnemonic) const
 						break;
 
 					case 0x6F:
-						(address, pMnemonic);
+						DecodeRLD(address, pMnemonic);
+						break;
+
+					case 0x80:
+					case 0x81:
+					case 0x82:
+					case 0x83:
+					case 0x84:
+					case 0x85:
+					case 0x86:
+					case 0x87:
+					case 0x88:
+					case 0x89:
+					case 0x8A:
+					case 0x8B:
+					case 0x8C:
+					case 0x8D:
+					case 0x8E:
+					case 0x8F:
+					case 0x90:
+					case 0x91:
+					case 0x92:
+					case 0x93:
+					case 0x94:
+					case 0x95:
+					case 0x96:
+					case 0x97:
+					case 0x98:
+					case 0x99:
+					case 0x9A:
+					case 0x9B:
+					case 0x9C:
+					case 0x9D:
+					case 0x9E:
+					case 0x9F:
+						DecodeNOP(address, pMnemonic);
 						break;
 
 					case 0xA0:
 						DecodeLDI(address, pMnemonic);
 						break;
 
-					case 0xA8:
-						DecodeLDD(address, pMnemonic);
-						break;
-
-					case 0xB0:
-						DecodeLDIR(address, pMnemonic);
-						break;
-
-					case 0xB8:
-						DecodeLDDR(address, pMnemonic);
-						break;
-
 					case 0xA1:
 						DecodeCPI(address, pMnemonic);
-						break;
-
-					case 0xA9:
-						DecodeCPD(address, pMnemonic);
-						break;
-
-					case 0xB1:
-						DecodeCPIR(address, pMnemonic);
-						break;
-
-					case 0xB9:
-						DecodeCPDR(address, pMnemonic);
 						break;
 
 					case 0xA2:
 						DecodeINI(address, pMnemonic);
 						break;
 
-					case 0xAA:
-						DecodeIND(address, pMnemonic);
-						break;
-
-					case 0xB2:
-						DecodeINIR(address, pMnemonic);
-						break;
-
-					case 0xBA:
-						DecodeINDR(address, pMnemonic);
-						break;
-
 					case 0xA3:
 						DecodeOUTI(address, pMnemonic);
+						break;
+
+					case 0xA4:
+					case 0xA5:
+					case 0xA6:
+					case 0xA7:
+						DecodeNOP(address, pMnemonic);
+						break;
+
+					case 0xA8:
+						DecodeLDD(address, pMnemonic);
+						break;
+
+					case 0xA9:
+						DecodeCPD(address, pMnemonic);
+						break;
+
+					case 0xAA:
+						DecodeIND(address, pMnemonic);
 						break;
 
 					case 0xAB:
 						DecodeOUTD(address, pMnemonic);
 						break;
 
+					case 0xAC:
+					case 0xAD:
+					case 0xAE:
+					case 0xAF:
+						DecodeNOP(address, pMnemonic);
+						break;
+
+					case 0xB0:
+						DecodeLDIR(address, pMnemonic);
+						break;
+
+					case 0xB1:
+						DecodeCPIR(address, pMnemonic);
+						break;
+
+					case 0xB2:
+						DecodeINIR(address, pMnemonic);
+						break;
+
 					case 0xB3:
 						DecodeOTIR(address, pMnemonic);
 						break;
 
+					case 0xB4:
+					case 0xB5:
+					case 0xB6:
+					case 0xB7:
+						DecodeNOP(address, pMnemonic);
+						break;
+
+					case 0xB8:
+						DecodeLDDR(address, pMnemonic);
+						break;
+
+					case 0xB9:
+						DecodeCPDR(address, pMnemonic);
+						break;
+
+					case 0xBA:
+						DecodeINDR(address, pMnemonic);
+						break;
+
 					case 0xBB:
 						DecodeOTDR(address, pMnemonic);
+						break;
+
+					case 0xBC:
+					case 0xBD:
+					case 0xBE:
+					case 0xBF:
+					case 0xC0:
+					case 0xC1:
+					case 0xC2:
+					case 0xC3:
+					case 0xC4:
+					case 0xC5:
+					case 0xC6:
+					case 0xC7:
+					case 0xC8:
+					case 0xC9:
+					case 0xCA:
+					case 0xCB:
+					case 0xCC:
+					case 0xCD:
+					case 0xCE:
+					case 0xCF:
+					case 0xD0:
+					case 0xD1:
+					case 0xD2:
+					case 0xD3:
+					case 0xD4:
+					case 0xD5:
+					case 0xD6:
+					case 0xD7:
+					case 0xD8:
+					case 0xD9:
+					case 0xDA:
+					case 0xDB:
+					case 0xDC:
+					case 0xDD:
+					case 0xDE:
+					case 0xDF:
+					case 0xE0:
+					case 0xE1:
+					case 0xE2:
+					case 0xE3:
+					case 0xE4:
+					case 0xE5:
+					case 0xE6:
+					case 0xE7:
+					case 0xE8:
+					case 0xE9:
+					case 0xEA:
+					case 0xEB:
+					case 0xEC:
+					case 0xED:
+					case 0xEE:
+					case 0xEF:
+					case 0xF0:
+					case 0xF1:
+					case 0xF2:
+					case 0xF3:
+					case 0xF4:
+					case 0xF5:
+					case 0xF6:
+					case 0xF7:
+					case 0xF8:
+					case 0xF9:
+					case 0xFA:
+					case 0xFB:
+					case 0xFC:
+					case 0xFD:
+					case 0xFE:
+					case 0xFF:
+						DecodeNOP(address, pMnemonic);
 						break;
 
 				default:
