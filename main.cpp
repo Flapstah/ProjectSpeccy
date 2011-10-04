78aa277e01b62145b0220f9a5c308c0902b65928
Flapstah <flapstah@gmail.com>
2011-10-04 21:54:17 +0100

    bit of sorting as to what tapes work

--- main.cpp
+++ main.cpp
@@ -21,15 +21,20 @@ int main(int argc, char* argv[])
 	speccy.LoadRAW("roms/z80tests.raw");
 	speccy.LoadTAP("roms/zexbit.tap");
 	speccy.LoadTAP("roms/zexfix.tap");
-
-	// not working
-	speccy.LoadSNA("roms/LORDSMID.SNA");
+	speccy.LoadTZX("roms/The Lords Of Midnight - Side 1.tzx");
+	speccy.LoadTZX("roms/Wheelie.tzx");
 	speccy.LoadRAW("roms/manic.raw");
 	speccy.LoadRAW("roms/jsw.raw");
-	speccy.LoadRAW("roms/007Spy.raw");
+
+	// needs work (loads but odd behaviour)
 	speccy.LoadRAW("roms/Magnetron.raw");
+	speccy.LoadTZX("roms/Magnetron.tzx");
+
+	// not working
+	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx"); // think there's a timing bug with block 0x11 but Magnetron loads block 0x11 OK...
+	speccy.LoadTZX("roms/Fairlight - 48k - Release 2.tzx");
 */
-	speccy.LoadTAP("roms/zexfix.tap");
+	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx"); // think there's a timing bug with block 0x11 but Magnetron loads block 0x11 OK...
 	while (speccy.Update());
 
 	return EXIT_SUCCESS;
