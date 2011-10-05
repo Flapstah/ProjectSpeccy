11dd8373c62a54e517ae8b3e704eacdc2c4134be
andyc <andyc@crytek.com>
2011-10-05 16:41:51 +0100

    more tapes working

--- main.cpp
+++ main.cpp
@@ -25,16 +25,12 @@ int main(int argc, char* argv[])
 	speccy.LoadTZX("roms/Wheelie.tzx");
 	speccy.LoadRAW("roms/manic.raw");
 	speccy.LoadRAW("roms/jsw.raw");
-
-	// needs work (loads but odd behaviour)
-	speccy.LoadRAW("roms/Magnetron.raw");
+	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx");
 	speccy.LoadTZX("roms/Magnetron.tzx");
 
 	// not working
-	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx"); // think there's a timing bug with block 0x11 but Magnetron loads block 0x11 OK...
-	speccy.LoadTZX("roms/Fairlight - 48k - Release 2.tzx");
 */
-	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx"); // think there's a timing bug with block 0x11 but Magnetron loads block 0x11 OK...
+	speccy.LoadTZX("roms/Fairlight - 48k - Release 1.tzx");
 	while (speccy.Update());
 
 	return EXIT_SUCCESS;
